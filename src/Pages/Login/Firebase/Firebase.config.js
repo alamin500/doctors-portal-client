324de57d03f40b65e0a23908e9da0dee7d719833
alamin500 <nalamin500@gmail.com>
2021-11-05 19:40:48 +0600

    h

---
+++ src/Pages/Login/Firebase/Firebase.config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+export default firebaseConfig;
+
+// REACT_APP_API_KEY
+// REACT_APP_AUTH_DOMAIN
+// REACT_APP_PROJECT_ID
+// REACT_APP_STORAGE_BUCKET
+// REACT_APP_MESSAGING_SENDERID
+// REACT_APP_APP_ID
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,82 @@
 import React from "react";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import login from "../../../images/login.png";
 
 const Register = () => {
+  const [loginData, setLoginData] = useState({});
+  const handleOnChange = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newLoginData = { ...loginData };
+    newLoginData[field] = value;
+    console.log(newLoginData);
+    setLoginData(newLoginData);
+  };
+
+  const handleLoginSubmit = (e) => {
+    if (loginData.password !== loginData.password2) {
+      alert("Your password did not match");
+      return;
+    }
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item sx={{ mt: 10 }} xs={12} md={6}>
+          <Typography variant="h6" gutterBottom component="div">
+            Register
+          </Typography>
+          <form onSubmit={handleLoginSubmit}>
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-basic"
+              type="email"
+              label="Your Email"
+              name="email"
+              onChange={handleOnChange}
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-password-input"
+              label="Password"
+              type="password"
+              name="password"
+              onChange={handleOnChange}
+              autoComplete="current-password"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-password-input"
+              label="Re-type password"
+              type="password"
+              name="password2"
+              onChange={handleOnChange}
+              autoComplete="current-password"
+              variant="standard"
+            />
+
+            <Button
+              sx={{ width: "75%", m: 1 }}
+              variant="contained"
+              type="submit"
+            >
+              Login
+            </Button>
+            <NavLink style={{ textDecoration: "none" }} to="/login">
+              <Button variant="text">Already Registered? please Login</Button>
+            </NavLink>
+          </form>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img style={{ width: "100%" }} src={login} alt="" />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
